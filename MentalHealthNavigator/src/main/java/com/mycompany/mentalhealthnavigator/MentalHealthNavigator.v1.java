048bcfefe34020be040b0ad144cc357546bb9709
Samuel Bissou <jbissou@htu.edu>
2025-11-29 21:08:32 -0600

    initial project setup
    initial project setup

---
+++ MentalHealthNavigator/src/main/java/com/mycompany/mentalhealthnavigator/MentalHealthNavigator.v1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mentalhealthnavigator;
+
+/**
+ *
+ * @author bisso
+ */
+public class MentalHealthNavigator {
+
+    public static void main(String[] args) {
+     
+    
+    }
+}
